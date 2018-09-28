93466572ec39ae82286b14f5f1a3183a65b272b5
Unknown <akhmadfauzan114@gmail.com>
2018-09-28 16:59:20 +0700

    Add 5 last transaction

--- app/TransactionHistoryScreen/sections/TransactionHistoriesList-test.js
+++ app/TransactionHistoryScreen/sections/TransactionHistoriesList-test.js
@@ -1,24 +1,24 @@
 /* global expect it describe */
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
 import TransactionHistoriesList from './TransactionHistoriesList';
 
 describe('TransactionHistoriesList', () => {
-  const transactions = [{
-    amount: 1220302,
-    dateTime: '2018-09-28T14:07:17.369',
-    transactionId: 'T00000006',
-    transactionType: 'credit',
-  }, {
-    amount: 2220302,
-    dateTime: '2018-09-28T14:07:17.369',
-    transactionId: 'T00000007',
-    transactionType: 'credit',
-  }];
-  it('should show two transaction in list and first one with key T00000006 ', () => {
-    const wrapper = shallow(<TransactionHistoriesList data={transactions} />);
-    expect(wrapper.find('#transactionsId').props().children.length).toBe(2);
-    expect(wrapper.find('#transactionsId').props().children[0].key).toBe('T00000006');
-  });
+    const transactions = [{
+        amount: 1220302,
+        dateTime: '2018-09-28T14:07:17.369',
+        transactionId: 'T00000006',
+        transactionType: 'credit',
+    }, {
+        amount: 2220302,
+        dateTime: '2018-09-28T14:07:17.369',
+        transactionId: 'T00000007',
+        transactionType: 'credit',
+    }];
+    it('should show two transaction in list and first one with key T00000006 ', () => {
+        const wrapper = shallow(<TransactionHistoriesList data={transactions}/>);
+        expect(wrapper.find('#transactionsId').props().children.length).toBe(2);
+        expect(wrapper.find('#transactionsId').props().children[0].key).toBe('T00000006');
+    });
 });
