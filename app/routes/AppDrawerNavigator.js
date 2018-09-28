6cea774b04f5bbadf597e366bebbbcd9c74f50e7
Unknown <akhmadfauzan114@gmail.com>
2018-09-28 13:54:42 +0700

    Add header home and drawer navigation

---
+++ app/routes/AppDrawerNavigator.js
@@ -0,0 +1,46 @@
+import { createDrawerNavigator, DrawerItems } from 'react-navigation';
+import {
+  ScrollView, SafeAreaView, Dimensions, View, StyleSheet, Image,
+} from 'react-native';
+import React from 'react';
+
+import HomeContainer from '../HomeScreen/views/HomeContainer';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  menu: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    height: 100,
+    width: 100,
+    borderRadius: 60,
+  },
+});
+
+const CustomDrawerComponent = props => (
+  <SafeAreaView style={styles.container}>
+    <ScrollView>
+      <View style={styles.menu}>
+        <Image style={styles.logo} source={require('../resources/images/zoomba-logo.png')} />
+      </View>
+      <DrawerItems {...props} />
+    </ScrollView>
+  </SafeAreaView>
+);
+
+
+const AppDrawerNavigator = createDrawerNavigator({
+  Home: HomeContainer,
+}, {
+  contentComponent: CustomDrawerComponent,
+  contentOptions: {
+    activeTintColor: '#3f9142',
+  },
+});
+
+export default AppDrawerNavigator;
