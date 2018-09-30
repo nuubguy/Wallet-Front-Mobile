77b6cf67e77088624e818419c2b64e3428bb4d5a
Unknown <akhmadfauzan114@gmail.com>
2018-10-01 06:56:02 +0700

    Modify title of header

--- app/routes/AppHeader.js
+++ app/routes/AppHeader.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import {Header} from "react-native-elements";
-import { View} from 'react-native';
+import {View} from 'react-native';
 
 const AppHeader = props => {
     return (
         <View>
             <Header
-                style={{paddingTop: 90}}
+                font
                 backgroundColor='#0fb9b1'
-                leftComponent={{ icon: 'menu', color: '#fff', onPress : () => props.data.navigation.openDrawer() }}
-                centerComponent={{ text: props.title, style: { color: '#fff' } }}
+                leftComponent={{icon: 'menu', color: '#fff', onPress: () => props.data.navigation.openDrawer()}}
+                centerComponent={{
+                    text: props.title,
+                    style: {
+                        color: '#fff',
+                        fontWeight: 'bold',
+                        fontSize : 16,
+                    }
+                }}
             />
         </View>
 
