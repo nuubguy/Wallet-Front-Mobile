9a2c6f8d0c04cf776efe9ee90c0a1847fc2e2424
Unknown <akhmadfauzan114@gmail.com>
2018-09-30 16:18:03 +0700

    Refactor TopUp form to transaction Form [reusable]

---
+++ app/TopUpScreen/sections/TransactionForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {View} from 'react-native';
+import {FormLabel, FormInput, Button} from 'react-native-elements'
+import * as stylesBase from "../../config/Base";
+import PropTypes from 'prop-types';
+
+// represent form for save money to wallet
+const TransactionForm = (props) => (
+    <View>
+        <View style={stylesBase.ROW}>
+            <FormInput
+                id="amount"
+                value={props.amount}
+                keyboardType='numeric'
+                placeholder={'Amount of Money'}
+                onChangeText={props.onChangeAmount}/>
+        </View>
+        <View style={stylesBase.ROW}>
+            <FormInput
+                id="description"
+                value={props.description}
+                placeholder={'Description (optional)'}
+                onChangeText={props.onChangeDescription}/>
+        </View>
+        <View style={stylesBase.ROW}>
+            <Button backgroundColor={'#33ad41'}
+                    id="btnConfirm"
+                    title='Confirm'
+                    onPress={props.onPressSubmit}
+                    disabled={props.valid}
+            />
+        </View>
+    </View>
+);
+
+export default TransactionForm;
+TransactionForm.propTypes = {
+    amount: PropTypes.string,
+    description: PropTypes.string,
+    onChangeAmount: PropTypes.func,
+    onChangeDescription: PropTypes.func,
+    onPressSubmit: PropTypes.func,
+};
