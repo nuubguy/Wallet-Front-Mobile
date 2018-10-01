f373628fc906460d4a1f33b2cc097a636508ba4e
Unknown <akhmadfauzan114@gmail.com>
2018-10-01 16:10:36 +0700

    Modify Height Header App

--- app/routes/AppHeader.js
+++ app/routes/AppHeader.js
@@ -7,6 +7,7 @@ const AppHeader = props => {
         <View>
             <Header
                 backgroundColor='#0fb9b1'
+                outerContainerStyles={{height: 90}}
                 leftComponent={{ icon: 'menu', color: '#fff', onPress : () => props.data.navigation.openDrawer() }}
                 centerComponent={{ text: props.title, style: { color: '#fff', fontSize: 18, fontWeight:'bold' } }}
             />
