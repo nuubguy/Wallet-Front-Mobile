4402b13aa1decc40a0c62d34da11bfeef507766f
Unknown <akhmadfauzan114@gmail.com>
2018-10-03 04:15:48 +0700

    [Fauzan, Nofanto] modify login styles

--- app/LoginScreen/sections/LoginForm.js
+++ app/LoginScreen/sections/LoginForm.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {View, Text, TextInput, TouchableOpacity, StyleSheet, StatusBar} from 'react-native';
+import {
+    View, Text, TextInput, TouchableOpacity, StyleSheet, StatusBar,
+} from 'react-native';
 
 // represent form to authenticate user
-const LoginForm = (props) => (
+const LoginForm = props => (
     <View style={styles.container}>
-        <StatusBar barStyle="light-content"/>
+        <StatusBar barStyle="light-content" />
         <TextInput
             id="email"
             style={styles.input}
             onSubmitEditing={() => this.passwordInput.focus()}
-            keyboardType='email-address'
+            keyboardType="email-address"
             returnKeyType="next"
-            placeholder='Enter Email'
+            placeholder="Enter Email"
+            placeholderTextColor="#fff"
             onChangeText={props.onChangeEmail}
             value={props.email}
         />
@@ -20,11 +23,14 @@ const LoginForm = (props) => (
         <TextInput
             id="password"
             style={styles.input}
-            returnKeyType="go" ref={(input) => this.passwordInput = input}
-            placeholder='Enter Password'
+            returnKeyType="go"
+            ref={input => this.passwordInput = input}
+            placeholder="Enter Password"
+            placeholderTextColor="#fff"
             onChangeText={props.onChangePassword}
             value={props.password}
-            secureTextEntry/>
+            secureTextEntry
+        />
         <TouchableOpacity style={styles.buttonContainer} id="btnLogin" onPress={props.onPressSubmit}>
             <Text style={styles.buttonText}>LOGIN</Text>
         </TouchableOpacity>
@@ -33,28 +39,29 @@ const LoginForm = (props) => (
 
 const styles = StyleSheet.create({
     container: {
-        padding: 20
+        padding: 20,
     },
     input: {
         height: 40,
-        backgroundColor: '#83812c',
+        backgroundColor: '#0d4745',
         marginBottom: 10,
         padding: 10,
-        color: '#fff'
+        color: '#fff',
+        borderRadius: 10,
     },
     buttonContainer: {
         backgroundColor: '#2980b6',
-        paddingVertical: 15
+        paddingVertical: 15,
     },
     buttonText: {
         color: '#fff',
         textAlign: 'center',
-        fontWeight: '700'
+        fontWeight: '700',
     },
     loginButton: {
         backgroundColor: '#2980b6',
-        color: '#fff'
-    }
+        color: '#fff',
+    },
 
 });
 
@@ -64,5 +71,5 @@ LoginForm.propTypes = {
     email: PropTypes.string,
     password: PropTypes.string,
     onChangeEmail: PropTypes.func,
-    onChangePassword: PropTypes.func
-}
+    onChangePassword: PropTypes.func,
+};
